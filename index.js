5d858c6be544e386ba23d340cf9f15d3a1547315
Colt <98503459+coltschultz@users.noreply.github.com>
2022-04-02 01:33:24 -0500

    employee and manager

---
+++ index.js
@@ -0,0 +1,11 @@
+const inquirer = require('inquirer');
+const Employee = require('./lib/Employee.js');
+const Manager = require('./lib/Manager.js');
+
+console.log('Welcome');
+
+new Manager().askOffice();
+
+
+
+
